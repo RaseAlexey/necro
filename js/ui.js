27caae3349c95b0c7c4b6d3b8900c3b3e1c1c975
Alexey <yaltarase@gmail.com>
2014-08-21 18:21:39 +0400

    fix

---
+++ js/ui.js
@@ -0,0 +1,10 @@
+
+open_fitting_room = function() {
+	$('.battle').hide();
+	$('.fitting_room').show();
+};
+start_battle = function() {
+	$('.fitting_room').hide();
+	$('.battle').show();
+};
+ open_fitting_room();
